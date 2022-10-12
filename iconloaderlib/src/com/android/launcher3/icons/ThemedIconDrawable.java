b8244916420ff8e49382d75637e31353212c24a7
Naveen <naveen.ccmsd@gmail.com>
2022-10-12 17:27:02 +0530

    Transparent background for themed icons (#7)
    * Added transparent background for icons
    
    * Rename method Name
    
    * Fix transparent background for clock
    
    * Cleanup `shouldTransparentBGIcons`
    
    * Cleanup `getThemedColors`
    
    Co-authored-by: Goooler <wangzongler@gmail.com>

--- iconloaderlib/src/com/android/launcher3/icons/ThemedIconDrawable.java
+++ iconloaderlib/src/com/android/launcher3/icons/ThemedIconDrawable.java
@@ -18,7 +18,6 @@ package com.android.launcher3.icons;
 import static android.content.res.Configuration.UI_MODE_NIGHT_MASK;
 import static android.content.res.Configuration.UI_MODE_NIGHT_YES;
 import static android.content.res.Resources.ID_NULL;
-
 import static com.android.launcher3.icons.GraphicsUtils.getExpectedBitmapSize;
 import static com.android.launcher3.icons.IconProvider.ICON_TYPE_CALENDAR;
 import static com.android.launcher3.icons.IconProvider.ICON_TYPE_CLOCK;
@@ -40,8 +39,10 @@ import android.os.Process;
 import android.os.UserHandle;
 import android.util.Log;
 
+import androidx.annotation.ColorInt;
 import androidx.annotation.Nullable;
 import androidx.arch.core.util.Function;
+import androidx.core.graphics.ColorUtils;
 
 import com.android.launcher3.icons.BitmapInfo.Extender;
 import com.android.launcher3.icons.cache.BaseIconCache;
@@ -54,6 +55,7 @@ import java.io.IOException;
 
 import app.lawnchair.icons.CustomAdaptiveIconDrawable;
 import app.lawnchair.icons.ExtendedBitmapDrawable;
+import app.lawnchair.icons.IconPreferencesKt;
 
 /**
  * Class to handle monochrome themed app icons
@@ -150,7 +152,7 @@ public class ThemedIconDrawable extends FastBitmapDrawable {
 
         @Override
         public FastBitmapDrawable newThemedIcon(Context context) {
-            int[] colors = getColors(context);
+            int[] colors = getThemedColors(context);
             FastBitmapDrawable drawable = new ThemedConstantState(this, colors[0], colors[1], false)
                     .newDrawable();
             drawable.mDisabledAlpha = GraphicsUtils.getFloat(context, R.attr.disabledIconAlpha, 1f);
@@ -296,7 +298,7 @@ public class ThemedIconDrawable extends FastBitmapDrawable {
 
         @Override
         public Drawable getThemedDrawable(Context context) {
-            int[] colors = getColors(context);
+            int[] colors = getThemedColors(context);
             Drawable bg = new ColorDrawable(colors[0]);
             float inset = getExtraInsetFraction() / (1 + 2 * getExtraInsetFraction());
             Drawable fg = new InsetDrawable(mThemeData.loadMonochromeDrawable(colors[1]), inset);
@@ -328,7 +330,7 @@ public class ThemedIconDrawable extends FastBitmapDrawable {
 
         @Override
         public Drawable getThemedDrawable(Context context) {
-            int[] colors = getColors(context);
+            int[] colors = getThemedColors(context);
             Drawable bg = new ColorDrawable(colors[0]);
             float extraInsetFraction = CustomAdaptiveIconDrawable.getExtraInsetFraction();
             float inset = extraInsetFraction / (1 + 2 * extraInsetFraction);
@@ -337,9 +339,24 @@ public class ThemedIconDrawable extends FastBitmapDrawable {
         }
     }
 
+    @ColorInt
+    public static int[] getThemedColors(Context context) {
+        final int[] result = getColors(context);
+        if (!IconPreferencesKt.shouldTransparentBGIcons(context)) {
+            return result;
+        }
+        if ((context.getResources().getConfiguration().uiMode & UI_MODE_NIGHT_MASK) != UI_MODE_NIGHT_YES) {
+            //Get Composite color for light mode or non dark mode
+            result[1] = ColorUtils.compositeColors(context.getResources().getColor(android.R.color.black), result[1]);
+        }
+        result[0] = 0;
+        return result;
+    }
+
     /**
      * Get an int array representing background and foreground colors for themed icons
      */
+    @ColorInt
     public static int[] getColors(Context context) {
         if (COLORS_LOADER != null) {
             return COLORS_LOADER.apply(context);
