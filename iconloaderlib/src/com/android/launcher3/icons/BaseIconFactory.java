ac8ecb72aa5e4f78aab021f158031e38d813ad39
MrSluffy <werdna.jac@gmail.com>
2024-12-14 16:00:38 +0800

    fixed masking when closing apps
    1. Set icon pack
    2. apps to home
    
    closes : #5080

--- iconloaderlib/src/com/android/launcher3/icons/BaseIconFactory.java
+++ iconloaderlib/src/com/android/launcher3/icons/BaseIconFactory.java
@@ -194,7 +194,9 @@ public class BaseIconFactory implements AutoCloseable {
      * @param color       used for the foreground text color
      */
     public BitmapInfo createIconBitmap(String placeholder, int color) {
-        AdaptiveIconDrawable drawable = new AdaptiveIconDrawable(
+        if (!ATLEAST_OREO) return null;
+
+        AdaptiveIconDrawable drawable = new CustomAdaptiveIconDrawable(
                 new ColorDrawable(PLACEHOLDER_BACKGROUND_COLOR),
                 new CenterTextDrawable(placeholder, color));
         Bitmap icon = createIconBitmap(drawable, IconNormalizer.ICON_VISIBLE_AREA_FACTOR);
@@ -215,10 +217,12 @@ public class BaseIconFactory implements AutoCloseable {
     @NonNull
     public BitmapInfo createShapedIconBitmap(Bitmap icon, IconOptions options) {
         Drawable d = new FixedSizeBitmapDrawable(icon);
-        float inset = getExtraInsetFraction();
-        inset = inset / (1 + 2 * inset);
-        d = new AdaptiveIconDrawable(new ColorDrawable(Color.BLACK),
-                new InsetDrawable(d, inset, inset, inset, inset));
+        if (ATLEAST_OREO) {
+            float inset = AdaptiveIconDrawable.getExtraInsetFraction();
+            inset = inset / (1 + 2 * inset);
+            d = new CustomAdaptiveIconDrawable(new ColorDrawable(Color.BLACK),
+                    new InsetDrawable(d, inset, inset, inset, inset));
+        }
         return createBadgedIconBitmap(d, options);
     }
 
@@ -240,8 +244,7 @@ public class BaseIconFactory implements AutoCloseable {
         float[] scale = new float[1];
         var adaptiveIcon = normalizeAndWrapToAdaptiveIcon(icon, null, scale);
 
-        Bitmap bitmap = createIconBitmap(adaptiveIcon, scale[0],
-                options == null ? MODE_WITH_SHADOW : options.mGenerationMode);
+        Bitmap bitmap = createIconBitmap(icon, scale[0]);
 
         if (ATLEAST_OREO && icon instanceof AdaptiveIconDrawable) {
             mCanvas.setBitmap(bitmap);
@@ -347,16 +350,12 @@ public class BaseIconFactory implements AutoCloseable {
               @Nullable final RectF outIconBounds,
               @NonNull final float[] outScale) {
 
-        boolean shrinkNonAdaptiveIcons = ATLEAST_OREO;
-
         if (icon == null) {
             return null;
         }
 
-        if (shrinkNonAdaptiveIcons) {
-            boolean isFromIconPack = ExtendedBitmapDrawable.isFromIconPack(icon);
-            shrinkNonAdaptiveIcons = !isFromIconPack && IconPreferencesKt.shouldWrapAdaptive(mContext);
-        }
+        boolean isFromIconPack = ExtendedBitmapDrawable.isFromIconPack(icon);
+        boolean shrinkNonAdaptiveIcons = !isFromIconPack && IconPreferencesKt.shouldWrapAdaptive(mContext);
 
         float scale;
 
@@ -421,15 +420,6 @@ public class BaseIconFactory implements AutoCloseable {
             badgeWithDrawable(c, new FixedSizeBitmapDrawable(badgeInfo.icon));
         });
         return BitmapInfo.of(icon, badgeInfo.color);
-    }
-
-    /**
-     * Adds the {@param badge} on top of {@param target} using the badge dimensions.
-     */
-    public void badgeWithDrawable(Bitmap target, Drawable badge) {
-        mCanvas.setBitmap(target);
-        badgeWithDrawable(mCanvas, badge);
-        mCanvas.setBitmap(null);
     }
 
     /**
