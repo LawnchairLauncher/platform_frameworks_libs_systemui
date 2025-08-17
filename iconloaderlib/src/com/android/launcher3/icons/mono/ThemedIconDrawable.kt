50e0eb9283de992be30ca014d5f0708567d368a9
Pun Butrach <pun.butrach@gmail.com>
2025-08-17 17:24:31 +0700

    Use PorterDuff filter for older device + COLORS_LOADER
    Signed-off-by: Pun Butrach <pun.butrach@gmail.com>

--- iconloaderlib/src/com/android/launcher3/icons/mono/ThemedIconDrawable.kt
+++ iconloaderlib/src/com/android/launcher3/icons/mono/ThemedIconDrawable.kt
@@ -15,13 +15,20 @@
  */
 package com.android.launcher3.icons.mono
 
+import android.annotation.ColorInt
 import android.content.Context
+import android.content.res.Configuration.UI_MODE_NIGHT_MASK
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.graphics.Bitmap
 import android.graphics.BlendMode.SRC_IN
 import android.graphics.BlendModeColorFilter
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
 import android.graphics.Rect
+import android.os.Build
+import androidx.core.graphics.ColorUtils
 import com.android.launcher3.icons.BitmapInfo
 import com.android.launcher3.icons.FastBitmapDrawable
 import com.android.launcher3.icons.R
@@ -34,12 +41,20 @@ class ThemedIconDrawable(constantState: ThemedConstantState) :
 
     // The foreground/monochrome icon for the app
     private val monoIcon = constantState.mono
-    private val monoFilter = BlendModeColorFilter(colorFg, SRC_IN)
+    private val monoFilter = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        BlendModeColorFilter(colorFg, SRC_IN)
+    } else {
+        PorterDuffColorFilter(colorFg, PorterDuff.Mode.SRC_IN)
+    }
     private val monoPaint =
         Paint(Paint.ANTI_ALIAS_FLAG or Paint.FILTER_BITMAP_FLAG).apply { colorFilter = monoFilter }
 
     private val bgBitmap = constantState.whiteShadowLayer
-    private val bgFilter = BlendModeColorFilter(colorBg, SRC_IN)
+    private val bgFilter = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        BlendModeColorFilter(colorBg, SRC_IN)
+    } else {
+        PorterDuffColorFilter(colorBg, PorterDuff.Mode.SRC_IN)
+    }
     private val mBgPaint =
         Paint(Paint.ANTI_ALIAS_FLAG or Paint.FILTER_BITMAP_FLAG).apply { colorFilter = bgFilter }
 
@@ -53,12 +68,23 @@ class ThemedIconDrawable(constantState: ThemedConstantState) :
         val alpha = if (mIsDisabled) (mDisabledAlpha * FULLY_OPAQUE).toInt() else FULLY_OPAQUE
         mBgPaint.alpha = alpha
         mBgPaint.setColorFilter(
-            if (mIsDisabled) BlendModeColorFilter(getDisabledColor(colorBg), SRC_IN) else bgFilter
+            if (mIsDisabled) if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                BlendModeColorFilter(getDisabledColor(colorBg), SRC_IN)
+            } else {
+                PorterDuffColorFilter(getDisabledColor(colorBg), PorterDuff.Mode.SRC_IN)
+            } else bgFilter,
         )
 
         monoPaint.alpha = alpha
         monoPaint.setColorFilter(
-            if (mIsDisabled) BlendModeColorFilter(getDisabledColor(colorFg), SRC_IN) else monoFilter
+            if (mIsDisabled) if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                BlendModeColorFilter(
+                    getDisabledColor(colorFg),
+                    SRC_IN,
+                )
+            } else {
+                PorterDuffColorFilter(getDisabledColor(colorFg), PorterDuff.Mode.SRC_IN)
+            } else monoFilter,
         )
     }
 
@@ -85,14 +111,38 @@ class ThemedIconDrawable(constantState: ThemedConstantState) :
     companion object {
         const val TAG: String = "ThemedIconDrawable"
 
+        @ColorInt
+        fun getThemedColors(context: Context): IntArray {
+            val result = getColors(context)
+            if (!IconPreferencesKt.shouldTransparentBGIcons(context)) {
+                return result
+            }
+            if ((context.getResources()
+                    .getConfiguration().uiMode and UI_MODE_NIGHT_MASK) !== UI_MODE_NIGHT_YES
+            ) {
+                //Get Composite color for light mode or non dark mode
+                result[1] = ColorUtils.compositeColors(
+                    context.getResources().getColor(android.R.color.black), result[1],
+                )
+            }
+            result[0] = 0
+            return result
+        }
+
         /** Get an int array representing background and foreground colors for themed icons */
         @JvmStatic
         fun getColors(context: Context): IntArray {
+            if (COLORS_LOADER != null) {
+                return COLORS_LOADER(context);
+            }
             val res = context.resources
             return intArrayOf(
                 res.getColor(R.color.themed_icon_background_color),
                 res.getColor(R.color.themed_icon_color),
             )
         }
+
+        @JvmStatic
+        var COLORS_LOADER: (Context) -> IntArray = { context -> getColors(context) }
     }
 }
