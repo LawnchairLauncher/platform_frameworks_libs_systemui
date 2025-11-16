6234d2447438becd3c6067af6723dd7a3c9361cd
Pun Butrach <pun.butrach@gmail.com>
2025-11-16 16:58:32 +0700

    feat: animationlib Android 16 QPR1

--- animationlib/src/com/android/app/animation/Animations.kt
+++ animationlib/src/com/android/app/animation/Animations.kt
@@ -18,7 +18,6 @@ package com.android.app.animation
 
 import android.animation.Animator
 import android.view.View
-import com.android.app.animation.Animations.Companion.setOngoingAnimation
 
 /** A static class for general animation-related utilities. */
 class Animations {
--- animationlib/src/com/android/app/animation/Interpolators.java
+++ animationlib/src/com/android/app/animation/Interpolators.java
@@ -58,14 +58,14 @@ public class Interpolators {
      * is disappearing e.g. when moving off screen.
      */
     public static final Interpolator EMPHASIZED_ACCELERATE = new PathInterpolator(
-        0.3f, 0f, 0.8f, 0.15f);
+            0.3f, 0f, 0.8f, 0.15f);
 
     /**
      * The decelerating emphasized interpolator. Used for hero / emphasized movement of content that
      * is appearing e.g. when coming from off screen
      */
     public static final Interpolator EMPHASIZED_DECELERATE = new PathInterpolator(
-        0.05f, 0.7f, 0.1f, 1f);
+            0.05f, 0.7f, 0.1f, 1f);
 
 
     public static final Interpolator EXAGGERATED_EASE;
@@ -98,21 +98,21 @@ public class Interpolators {
      * The standard interpolator that should be used on every normal animation
      */
     public static final Interpolator STANDARD = new PathInterpolator(
-        0.2f, 0f, 0f, 1f);
+            0.2f, 0f, 0f, 1f);
 
     /**
      * The standard accelerating interpolator that should be used on every regular movement of
      * content that is disappearing e.g. when moving off screen.
      */
     public static final Interpolator STANDARD_ACCELERATE = new PathInterpolator(
-        0.3f, 0f, 1f, 1f);
+            0.3f, 0f, 1f, 1f);
 
     /**
      * The standard decelerating interpolator that should be used on every regular movement of
      * content that is appearing e.g. when coming from off screen.
      */
     public static final Interpolator STANDARD_DECELERATE = new PathInterpolator(
-        0f, 0f, 0f, 1f);
+            0f, 0f, 0f, 1f);
 
     /*
      * ============================================================================================
@@ -158,7 +158,7 @@ public class Interpolators {
      * goes from 1 to 0 instead of 0 to 1).
      */
     public static final Interpolator FAST_OUT_SLOW_IN_REVERSE =
-        new PathInterpolator(0.8f, 0f, 0.6f, 1f);
+            new PathInterpolator(0.8f, 0f, 0.6f, 1f);
     public static final Interpolator SLOW_OUT_LINEAR_IN = new PathInterpolator(0.8f, 0f, 1f, 1f);
     public static final Interpolator AGGRESSIVE_EASE = new PathInterpolator(0.2f, 0f, 0f, 1f);
     public static final Interpolator AGGRESSIVE_EASE_IN_OUT = new PathInterpolator(0.6f,0, 0.4f, 1);
@@ -182,31 +182,31 @@ public class Interpolators {
     public static final Interpolator CUSTOM_40_40 = new PathInterpolator(0.4f, 0f, 0.6f, 1f);
     public static final Interpolator ICON_OVERSHOT = new PathInterpolator(0.4f, 0f, 0.2f, 1.4f);
     public static final Interpolator ICON_OVERSHOT_LESS = new PathInterpolator(0.4f, 0f, 0.2f,
-        1.1f);
+            1.1f);
     public static final Interpolator PANEL_CLOSE_ACCELERATED = new PathInterpolator(0.3f, 0, 0.5f,
-        1);
+            1);
     public static final Interpolator BOUNCE = new BounceInterpolator();
     /**
      * For state transitions on the control panel that lives in GlobalActions.
      */
     public static final Interpolator CONTROL_STATE = new PathInterpolator(0.4f, 0f, 0.2f,
-        1.0f);
+            1.0f);
 
     /**
      * Interpolator to be used when animating a move based on a click. Pair with enough duration.
      */
     public static final Interpolator TOUCH_RESPONSE =
-        new PathInterpolator(0.3f, 0f, 0.1f, 1f);
+            new PathInterpolator(0.3f, 0f, 0.1f, 1f);
 
     /**
      * Like {@link #TOUCH_RESPONSE}, but used in case the animation is played in reverse (i.e. t
      * goes from 1 to 0 instead of 0 to 1).
      */
     public static final Interpolator TOUCH_RESPONSE_REVERSE =
-        new PathInterpolator(0.9f, 0f, 0.7f, 1f);
+            new PathInterpolator(0.9f, 0f, 0.7f, 1f);
 
     public static final Interpolator TOUCH_RESPONSE_ACCEL_DEACCEL =
-        v -> ACCELERATE_DECELERATE.getInterpolation(TOUCH_RESPONSE.getInterpolation(v));
+            v -> ACCELERATE_DECELERATE.getInterpolation(TOUCH_RESPONSE.getInterpolation(v));
 
 
     /**
@@ -236,7 +236,7 @@ public class Interpolators {
          */
         private float zInterpolate(float input) {
             return (1.0f - FOCAL_LENGTH / (FOCAL_LENGTH + input)) /
-                (1.0f - FOCAL_LENGTH / (FOCAL_LENGTH + 1.0f));
+                    (1.0f - FOCAL_LENGTH / (FOCAL_LENGTH + 1.0f));
         }
     };
 
@@ -297,13 +297,13 @@ public class Interpolators {
      * @return the interpolated overshoot
      */
     public static float getOvershootInterpolation(float progress, float overshootAmount,
-        float overshootStart) {
+            float overshootStart) {
         if (overshootAmount == 0.0f || overshootStart == 0.0f) {
             throw new IllegalArgumentException("Invalid values for overshoot");
         }
         float b = MathUtils.log((overshootAmount + 1) / (overshootAmount)) / overshootStart;
         return MathUtils.max(0.0f,
-            (float) (1.0f - Math.exp(-b * progress)) * (overshootAmount + 1.0f));
+                (float) (1.0f - Math.exp(-b * progress)) * (overshootAmount + 1.0f));
     }
 
     /**
@@ -344,11 +344,11 @@ public class Interpolators {
      * 1 by upperBound.
      */
     public static Interpolator clampToProgress(Interpolator interpolator, float lowerBound,
-        float upperBound) {
+            float upperBound) {
         if (upperBound < lowerBound) {
             throw new IllegalArgumentException(
-                String.format("upperBound (%f) must be greater than lowerBound (%f)",
-                    upperBound, lowerBound));
+                    String.format("upperBound (%f) must be greater than lowerBound (%f)",
+                            upperBound, lowerBound));
         }
         return t -> clampToProgress(interpolator, t, lowerBound, upperBound);
     }
@@ -361,11 +361,11 @@ public class Interpolators {
      * interpolator.
      */
     public static float clampToProgress(
-        Interpolator interpolator, float progress, float lowerBound, float upperBound) {
+            Interpolator interpolator, float progress, float lowerBound, float upperBound) {
         if (upperBound < lowerBound) {
             throw new IllegalArgumentException(
-                String.format("upperBound (%f) must be greater than lowerBound (%f)",
-                    upperBound, lowerBound));
+                    String.format("upperBound (%f) must be greater than lowerBound (%f)",
+                            upperBound, lowerBound));
         }
 
         if (progress == lowerBound && progress == upperBound) {
@@ -398,7 +398,7 @@ public class Interpolators {
      * such as to take over a user-controlled animation when they let go.
      */
     public static Interpolator mapToProgress(Interpolator interpolator, float lowerBound,
-        float upperBound) {
+            float upperBound) {
         return t -> mapRange(interpolator.getInterpolation(t), lowerBound, upperBound);
     }
 
