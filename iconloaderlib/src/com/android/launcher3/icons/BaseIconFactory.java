6a11ef767998885838a599331b5485f768b3d725
Pun Butrach <pun.butrach@gmail.com>
2026-02-22 18:19:37 +0700

    fix: Icon shadow can't be disabled by user preference
    Reimplement icon shadow toggle
    
    Signed-off-by: Pun Butrach <pun.butrach@gmail.com>

--- iconloaderlib/src/com/android/launcher3/icons/BaseIconFactory.java
+++ iconloaderlib/src/com/android/launcher3/icons/BaseIconFactory.java
@@ -10,6 +10,7 @@ import static com.android.launcher3.icons.IconProvider.ATLEAST_T;
 import static com.android.launcher3.icons.IconProvider.ICON_TYPE_DEFAULT;
 import static com.android.launcher3.icons.ShadowGenerator.BLUR_FACTOR;
 
+import static com.android.launcher3.icons.ShadowGenerator.ENABLE_SHADOWS;
 import static java.lang.annotation.RetentionPolicy.SOURCE;
 
 import android.annotation.TargetApi;
@@ -143,6 +144,7 @@ public class BaseIconFactory implements AutoCloseable {
     public ShadowGenerator getShadowGenerator() {
         if (mShadowGenerator == null) {
             mShadowGenerator = new ShadowGenerator(mIconBitmapSize);
+            ENABLE_SHADOWS = IconPreferencesKt.shouldShadowBGIcons(mContext);
         }
         return mShadowGenerator;
     }
